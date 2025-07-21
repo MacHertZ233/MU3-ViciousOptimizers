475ad31ea84b0e3d2cf1a2ca1b30430e0a3cc53a
MacHertZ <hhzixuan@126.com>
2025-07-21 18:31:50 +0800

    add HideStats not tested yet

---
+++ HideStats_plugin/Patches/BattleUIPatch.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using MU3;
+using UnityEngine;
+
+namespace HideStats_plugin.Patches
+{
+    [HarmonyPatch(typeof(BattleUI))]
+    public class BattleUIPatch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch("Awake")]
+        static void AwakePostfix(ref UIPlayerInfo ____playerInfo, ref UIEnemyInfo ____enemyInfo, ref UIEnemyDamage ____enemyDamageInfo)
+        {
+            ____playerInfo.gameObject.SetActive(false);
+            ____enemyInfo.gameObject.SetActive(false);
+            ____enemyDamageInfo.gameObject.SetActive(false);
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch("Update")]
+        static bool UpdatePrefix(ref UIJudge ____judgeInfo)
+        {
+            ____judgeInfo.execute();
+            return false;
+        }
+    }
+}
---
+++ HideStats_plugin/Plugin.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using HarmonyLib;
+using HideStats_plugin.Patches;
+
+namespace HideStats_plugin
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class HideStatsPlugin : BaseUnityPlugin
+    {
+        const string modGUID = "machertz233.hidestats";
+        const string modName = "HideStats";
+        const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+        internal BepInEx.Logging.ManualLogSource logger;
+
+        void Awake()
+        {
+            logger = BepInEx.Logging.Logger.CreateLogSource(modName);
+            harmony.PatchAll(typeof(HideStatsPlugin));
+            harmony.PatchAll(typeof(BattleUIPatch));
+
+            logger.LogInfo($"Plugin {modName} is loaded!");
+        }
+    }
+}
