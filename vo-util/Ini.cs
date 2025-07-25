40666a75b3b002c59b85b1dc70423f211bc72c6c
MacHertZ <hhzixuan@126.com>
2025-07-25 15:21:08 +0800

    encapsulate ini reading function

---
+++ vo-util/Ini.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace VO
+{
+	public class IniParser
+	{
+        [System.Runtime.InteropServices.DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(
+            string section,
+            string key,
+            string def,
+            System.Text.StringBuilder retVal,
+            int size,
+            string filePath);
+
+        string iniPath;
+
+        public IniParser(string path)
+		{
+            this.iniPath = path;
+		}
+
+        public string ReadValue(string section, string key, string def = "")
+        {
+            System.Text.StringBuilder retVal = new System.Text.StringBuilder(255);
+            GetPrivateProfileString(section, key, def, retVal, retVal.Capacity, iniPath);
+            return retVal.ToString();
+        }
+    }
+}
+
